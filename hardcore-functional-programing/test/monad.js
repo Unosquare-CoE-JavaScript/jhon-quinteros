364bd76fb032a814177c243c608c53594b6b4b3c
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-05 10:39:46 -0400

    solved the exercises of monad

--- hardcore-functional-programing/test/monad.js
+++ hardcore-functional-programing/test/monad.js
@@ -69,5 +69,60 @@ test("Example 2: reading file", assert => {
       .fold(() => 8080, x => x)
   
   assert.equal(getPort(), 3000);
-
 });
+
+/**
+ * Exercise 1: Refactor streetName to use Either instead of nested if's
+ * =========================
+ */
+test("Exercise 1: street", assert => {
+  const user = { address: { street: { name: "Willow" } } };
+  const street = user =>
+    fromNullable(user.address)
+    .map(address => address.street)
+    .fold(() => "no street", street => street)
+
+  assert.deepEqual(street(user), {name: "Willow"})
+  assert.equal(street({}), "no street")
+ })
+
+ /**
+  * Exercise 2: Refactor parseDbUrl to return an Either instead of try/catch
+  * =========================
+  */
+  const DB_REGEX = /postgres:\/\/([^:]+):([^@]+)@.*?\/(.+)$/i;
+  const parseDbUrl_ = cfg => {
+    try {
+      const c = JSON.parse(cfg) // throws if it can't parse
+      return c.url.match(DB_REGEX)
+    } catch(e) {
+      return null
+    }
+  }
+ const parseDbUrl = cfg =>
+  parseJSON(cfg)
+  .map(c => c.url.match(DB_REGEX))
+  .fold(() => null,x => x);
+
+ test("Exercise 2: parseDbUrl", assert => {
+  const config = '{"url": "postgres://sally:muppets@localhost:5432/mydb"}' 
+  assert.equal(parseDbUrl(config)[1], "sally")
+  assert.equal(parseDbUrl(), null)
+ })
+
+
+
+/**
+ * Exercise 3: Using Either and the functions above, refactor startApp
+ * =========================
+ */
+  test("Ex3: startApp", assert => {
+    const startApp = cfg =>
+      fromNullable(parseDbUrl(cfg))
+      .map(([_, user, password, db]) => `starting ${db}, ${user}, ${password}`)
+      .fold(() => "can't get config", x => x)
+
+    const config = '{"url": "postgres://sally:muppets@localhost:5432/mydb"}'
+    assert.equal(String(startApp(config)), "starting mydb, sally, muppets")
+    assert.equal(String(startApp()), "can't get config")
+  })
