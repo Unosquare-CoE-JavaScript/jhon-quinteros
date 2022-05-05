fd95f481def2f445a4a85df1e63569d49a310c0f
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-05 17:10:13 -0400

    added basic unit tests for the button color

--- testing-react-with-jest-and-testing-library/color-button/src/App.test.tsx
+++ testing-react-with-jest-and-testing-library/color-button/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+test('button has the correct initial color', () => {
+  render(<App/>);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toHaveClass('button-color-red');
 });
+
+test('button turns blue when clicked', () => {
+  render(<App/>);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveClass('button-color-blue');
+  expect(colorButton.textContent).toBe('Change to red');
+})
--- testing-react-with-jest-and-testing-library/color-button/src/App.tsx
+++ testing-react-with-jest-and-testing-library/color-button/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
 function App() {
+  const [isRed, setColor] = useState(true);
+
+  function onClickColorBtn() {
+    setColor(!isRed);
+  }
+
   return (
     <div>
+      <button className={isRed ? 'button-color-red' : 'button-color-blue'} onClick={onClickColorBtn}>Change to {isRed ? 'blue' : 'red'}</button>
     </div>
   );
 }
