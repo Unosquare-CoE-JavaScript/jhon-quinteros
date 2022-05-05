f43094eef7cbdbbb2d9ecd206f6c2adc944a65bd
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-05 11:37:09 -0400

    solved the task exercises

--- hardcore-functional-programing/test/task.js
+++ hardcore-functional-programing/test/task.js
@@ -57,3 +57,71 @@ test("Example 2: reading file", assert => {
   app().fork(() => content = "error", () => content = "success");
   assert.equal(content, '');
 });
+
+
+// SETUP
+// =========================
+const posts = {1: {title: "First"}, 2: {title: "Second"}}
+
+const comments = {First: [{id: 1, body: "Brilliant!"}], Second: [{id: 2, body: "Unforgivable"}]}
+
+const getPost = id =>
+  Task((rej, res) =>
+    setTimeout(() => posts[id] ? res(posts[id]) : rej('not found'), 200))
+
+const getComments = post =>
+  Task((rej, res) =>
+    setTimeout(() => res(comments[post.title]), 200))
+
+// Ex1: Use the result of getPost() and upperCase the title. Posts and comments are defined above and look like {title: String} and {id: Int, body: String} respectively.
+// =========================
+const postTitle = id => // uppercase the title of the result of getPost()
+  getPost(id)
+
+
+test("Ex1: postTitle", assert => {
+  const done = assert.async();
+  postTitle(1)
+  .map(post => post.title.toUpperCase())
+  .fork(console.error, t => {
+    assert.deepEqual(t, 'FIRST');
+    done();
+  })
+});
+
+// Ex2: pass in the post to getComments(), defined above, then assign the returned comments to the post
+// =========================
+const commentsForPost = id =>
+  getPost(id)
+
+test("Ex2: commentsForPost", assert => {
+  const done = assert.async();
+  commentsForPost(2)
+  .chain(post => 
+    getComments(post)
+    .map(comments =>
+      Object.assign({comments}, post)
+    )
+  )  
+  .fork(console.error, t => {
+    assert.deepEqual(t.title, "Second")
+    assert.deepEqual(t.comments, comments["Second"])
+    done()
+  })
+});
+
+// Ex3: Wrap location.href in a Task to make it "pure"
+// =========================
+const getHref =
+  Task((rej, res) => res("cdpn.io"))
+
+
+test("Ex3: getHref", assert => {
+  const done = assert.async();
+  getHref
+  .fork(console.error, t => {
+    assert.equal(true, !!t.match("cdpn.io"))
+    done()
+  })
+  
+})
