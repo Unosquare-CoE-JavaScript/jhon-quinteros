4087054f936b57713f048fc0e508ab2ebc0ca0cc
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-06 16:31:04 -0400

    added the unit tests for the popover

--- testing-react-with-jest-and-testing-library/sundaes-on-demand/src/pages/summary/SummaryForm.tsx
+++ testing-react-with-jest-and-testing-library/sundaes-on-demand/src/pages/summary/SummaryForm.tsx
@@ -1,31 +1,41 @@
 import React, { ChangeEvent, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import "./SummaryForm.css";
 
 export function SummaryForm(props: any) {
-  const [checked, setChecked] = useState(false);
+  const [isChecked, setChecked] = useState(false);
 
   function onClickCheck(e: ChangeEvent<HTMLInputElement>) {
     setChecked(e.target.checked);
   }
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
 
   return (
     <Form>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
-          checked={checked}
+          checked={isChecked}
           onChange={onClickCheck}
           label={
             <span>
               I agree to
-              <span className="terms-color"> Terms and Conditions</span>
+              <OverlayTrigger placement="right" overlay={popover}>
+                <span className="terms-color"> Terms and Conditions</span>
+              </OverlayTrigger>
             </span>
           }
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={!checked}>
+      <Button variant="primary" type="submit" disabled={!isChecked}>
         Confirm order
       </Button>
     </Form>
--- testing-react-with-jest-and-testing-library/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
+++ testing-react-with-jest-and-testing-library/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import { SummaryForm } from './../SummaryForm';
-import { fireEvent, render, screen } from '@testing-library/react';
+import React from "react";
+import { SummaryForm } from "./../SummaryForm";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('checkbox is unchecked by default', () => {
-  render(<SummaryForm/>);
-  const termsCheckbox = screen.getByRole('checkbox', {name: 'I agree to Terms and Conditions'});
-  const orderButton = screen.getByRole('button', {name: 'Confirm order'});
+test("checkbox is unchecked by default", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  const termsCheckbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const orderButton = screen.getByRole("button", { name: /confirm order/i });
 
   expect(termsCheckbox).not.toBeChecked();
-  expect(orderButton).toBeDisabled()
+  expect(orderButton).toBeDisabled();
 
-  fireEvent.click(termsCheckbox);
-
+  await user.click(termsCheckbox);
   expect(termsCheckbox).toBeChecked();
   expect(orderButton).toBeEnabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  const user = userEvent.setup();
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  await user.hover(termsAndConditions);
+  const termsPopover = screen.getByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(termsPopover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
