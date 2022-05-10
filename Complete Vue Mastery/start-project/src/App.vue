193d7bf125ea30a589e710247f8a06adc95137aa
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-10 17:25:47 -0400

    created the base project and added example code

---
+++ Complete Vue Mastery/start-project/src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      firstName: "pepe",
+      lastName: "Doe"
+    };
+  },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      {{fullName()}}
+      <hr/>
+      <label>First Name</label>
+      <input type="text" v-model="firstName"/>
+    </div>
+  </header>
+</template>
---
+++ Complete Vue Mastery/start-project/src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+const vm: any = app.mount("#app");
+
+setTimeout(() => {
+  vm.firstName = "Luis";
+},2000)
