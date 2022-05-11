fbcde52df0f70567a7c43f582ffcb0d21f0ad121
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-11 10:47:35 -0400

    added examples of two way biding, methods, computed values, watch, directives and event modifiers

--- Complete Vue Mastery/start-project/src/App.vue
+++ Complete Vue Mastery/start-project/src/App.vue
@@ -3,24 +3,60 @@ export default {
   data() {
     return {
       firstName: "pepe",
-      lastName: "Doe"
+      lastName: "Doe",
+      middleName: "",
+      age: 20,
     };
   },
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    decrease() {
+      this.age--;
+    },
+    increase() {
+      this.age++;
+    },
+    updateLastName(message: string, e: Event) {
+      console.log(message);
+      this.lastName = (e.target as HTMLInputElement).value;
+    },
+    updateMiddleName(event: Event) {
+      this.middleName = (event.target as HTMLInputElement).value;
     },
   },
+  computed: {
+    fullName() {
+      console.log("computed: ");
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
+    },
+  },
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        console.log("watch")
+        this.age = 20
+      }, 3000);
+    }
+  }
 };
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      {{fullName()}}
+      {{fullName}}
       <hr/>
       <label>First Name</label>
-      <input type="text" v-model="firstName"/>
+      <input type="text" v-model.lazy.trim="firstName"/>
+      <label>Middle Name</label>
+      <input type="text" @keyup.enter="updateMiddleName"/>
+      <label>Last Name</label>
+      <input type="text" :value="lastName" @input.prevent="updateLastName('Last event triggered!', $event)"/>
+      <br/>
+      <label>Age</label>
+      <input type="number" v-model.number="age"/>
+      <button type="button" @click.ctrl="increase">Increment</button>
+      <button type="button" v-on:click="decrease">Decrement</button>
+      <p>{{ age }}</p>
     </div>
   </header>
 </template>
