ca312dd05716525cbbe51501afc1cb0738602f1e
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-11 17:47:22 -0400

    added the lifecicle methods

--- Complete Vue Mastery/start-project/src/App.vue
+++ Complete Vue Mastery/start-project/src/App.vue
@@ -65,6 +65,30 @@ export default {
       }, 3000);
     },
   },
+  beforeCreate() {
+    console.log("beforeCreate function called", this.people);
+  },
+  created() {
+    console.log("created function called", this.people);
+  },
+  beforeMount() {
+    console.log("beforeMount function called", this.$el);
+  },
+  mounted() {
+    console.log("mounted function called", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate function called");
+  },
+  updated() {
+    console.log("updated function called");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount function called");
+  },
+  unmounted() {
+    console.log("unmounted function called");
+  },
 };
 </script>
 
