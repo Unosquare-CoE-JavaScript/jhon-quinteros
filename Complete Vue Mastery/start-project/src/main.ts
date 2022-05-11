94ce309dbdd8b9aae8b66b6a70cb48d2c43047cc
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-11 17:58:25 -0400

    created a component

--- Complete Vue Mastery/start-project/src/main.ts
+++ Complete Vue Mastery/start-project/src/main.ts
@@ -4,6 +4,15 @@ import App from "./App.vue";
 
 const app = createApp(App);
 
+export default app.component("hello-component", {
+  data() {
+    return {
+      message: "Hello world"
+    }
+  },
+  template: `<h1>{{message}}</h1>`
+});
+
 const vm: any = app.mount("#app");
 
 setTimeout(() => {
