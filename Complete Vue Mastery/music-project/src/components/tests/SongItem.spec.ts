4721ae002f71dda609083d6e129a780002d06cff
Jhon Quinteros <jhon.quinteros@unosquare.com>
2022-05-24 17:46:57 -0400

    added unit test for attributes

--- Complete Vue Mastery/music-project/src/components/tests/SongItem.spec.ts
+++ Complete Vue Mastery/music-project/src/components/tests/SongItem.spec.ts
@@ -3,6 +3,7 @@ import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import SongItem from "@/components/SongItem.vue";
 
 describe("SongItem.vue", () => {
+
   test("render song, display name", () => {
     const song = {
       docId: "1",
@@ -22,5 +23,25 @@ describe("SongItem.vue", () => {
     });
     const displayName = wrapper.get(".text-gray-500");
     expect(displayName.text()).toBe("Test song");
+  });
+
+  test("render song.docId in if attribute", () => {
+    const song = {
+      docId: "1",
+      modifiedName: "Test song",
+      displayName: "Test song",
+      commentCount: 0,
+    };
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song,
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+    expect(wrapper.attributes("id")).toBe("song-id-1");
   });
 });
